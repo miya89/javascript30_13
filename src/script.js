426b245a5cd5ee7d2e51eb8c5a313050b8dca10c
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-10 17:30:05 +0100

    Add script

---
+++ src/script.js
@@ -0,0 +1,40 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  sliderImages.forEach((sliderImage) => {
+    //halfway through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+
+    //the bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
